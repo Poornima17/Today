b20278cadda513da93536e64c36fe4a9c962657a
registered <poorniragav@gmail.com>
2020-04-16 12:56:06 +0530

    open comit

---
+++ src/org/cloning/dis.java
@@ -0,0 +1,11 @@
+package org.cloning;
+
+public class dis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("nthk to say");
+System.out.println("=======");
+	}
+
+}
