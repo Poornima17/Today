ccd0bf6fa7e25218551a49780feb7b38ed053fd4
registered <poorniragav@gmail.com>
2020-04-16 13:06:31 +0530

    first time

--- src/org/cloning/dis.java
+++ src/org/cloning/dis.java
@@ -5,7 +5,7 @@ public class dis {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("nthk to say");
-System.out.println("=======");
+System.out.println("=");
 	}
 
 }
